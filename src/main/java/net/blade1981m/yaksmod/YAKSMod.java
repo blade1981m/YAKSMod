8cbaec11d04aacca8294d5b66bf4a2028c8ee56b
blade1981m <blade1981m@gmail.com>
2016-07-03 15:24:01 -0500

    Created initial files
    Created initial class file and mcmod.info file.

---
+++ src/main/java/net/blade1981m/yaksmod/YAKSMod.java
@@ -0,0 +1,32 @@
+package net.blade1981m.yaksmod;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+/**
+ * Created by blade1981m on 7/3/2016.
+ */
+
+@Mod(modid = "YAKSMod", name = "YAKS Mod", version = "1.7.10-1.0")
+public class YAKSMod {
+
+    @Mod.Instance("YAKSMod")
+    public static YAKSMod instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void  postInit(FMLPostInitializationEvent event) {
+
+    }
+}
