65ebefcc517ec4fdabaafca610d5c0bcab8fdd31
blade1981m <blade1981m@gmail.com>
2016-10-01 16:21:15 -0500

    Updated Carbon Fragments recipes

---
+++ src/main/java/net/blade1981m/yaksmod/reference/Reference.java
@@ -0,0 +1,12 @@
+package net.blade1981m.yaksmod.reference;
+
+/**
+ * Created by blade1981m on 7/3/2016.
+ */
+
+public class Reference {
+
+    public static final String MOD_ID = "YAKSMod";
+    public static final String MOD_NAME = "YAKS Mod";
+    public static final String VERSION = "1.7.10-1.0";
+}
